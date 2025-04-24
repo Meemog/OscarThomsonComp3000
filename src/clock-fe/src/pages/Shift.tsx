a67f8fbfd56e7ae9cb5acf321660fc9c785a6dc4
Oscar <oscmog@gmail.com>
2025-04-24 15:20:52 +0100

    safety

--- src/clock-fe/src/pages/Shift.tsx
+++ src/clock-fe/src/pages/Shift.tsx
@@ -50,7 +50,8 @@ export default function Shift({tempToken}: {tempToken: string}): ReactNode {
         .then((response) => {
             if (response.status === 200){
                 response.json().then((data) => {
-                    console.log(data.shift)
+                    console.log(data)
+                    setContent(<UnclockedShift startTime={data.shift.startTime} endTime={data.shift.endTime} breakDur={data.shift.breakDuration}/>)
                 })
             } else {
                 setContent(
@@ -66,4 +67,46 @@ export default function Shift({tempToken}: {tempToken: string}): ReactNode {
     }
 
     return content
+}
+
+function UnclockedShift({startTime, endTime, breakDur}: {startTime: number, endTime: number, breakDur: number}): ReactNode {
+    const start = new Date(startTime).getHours() + ":" + new Date(startTime * 1000).getMinutes()
+    const end = new Date(endTime).getHours() + ":" + new Date(endTime * 1000).getMinutes()
+    console.log(startTime, endTime, breakDur)
+
+    function formatBreakDuration(breakDur: number): string {
+        if (breakDur === 0) return "No break"
+        const hours = Math.floor(breakDur / 3600)
+        const minutes = Math.floor((breakDur % 3600) / 60)
+        return `${hours}h ${minutes}m`
+    }
+
+    return (
+        <div className="mx-auto w-full max-w-md mt-3">
+            <h1 className="text-2xl text-center">Shift</h1>
+            <div className="grid grid-cols-3 gap-4 mt-3">
+                <p className="text-center">Start Time</p>
+                <p className="text-center">End Time</p>
+                <p className="text-center">Break Duration</p>
+                {startTime > new Date().getTime() ? <p className="text-center">{start}</p> : <p className="text-center bg-red-400 rounded">{start}</p>}
+                <p className="text-center">{end}</p>
+                <p className="text-center">{formatBreakDuration(breakDur)}</p>
+            </div>
+            <div className="flex justify-center mt-3">
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={() => {}}>Cancel</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={() => {}}>Clock In</button>
+            </div>
+        </div>
+    )
+}
+
+function ClockedShift(): ReactNode {
+    return (
+        <div className="mx-auto w-full max-w-md mt-3">
+            <h1 className="text-2xl text-center">Shift</h1>
+            <div className="bg-green-400 p-2 rounded">
+                <h1 className="text-xl text-center">You are in a shift</h1>
+            </div>
+        </div>
+    )
 }
